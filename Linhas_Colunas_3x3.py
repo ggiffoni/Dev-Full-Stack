28beb042de284794abf90fb598e8e197d2f9243e
ggiffoni <89454421+ggiffoni@users.noreply.github.com>
2023-08-13 08:11:28 -0300

    Exemplo de módulo - Colunas e Linhas 3x3

--- Linhas_Colunas_3x3.py
+++ Linhas_Colunas_3x3.py
@@ -1,4 +1,4 @@
-#Exemplo de módulo - Colunas 3x3
+#Exemplo de módulo - Colunas e Linhas 3x3
 
 print("Módulo importado com sucesso.")
 
@@ -23,5 +23,25 @@ def terceira_coluna_3x3(matriz_A, matriz_B):
     terceira = [[m13], [m23], [m33]]
     return terceira
 
+def primeira_linha_3x3(matriz_A, matriz_B):
+    m11 = matriz_A[0][0] * matriz_B[0][0] + matriz_A[0][1] * matriz_B[1][0] + matriz_A[0][2] * matriz_B[2][0]
+    m12 = matriz_A[0][0] * matriz_B[0][1] + matriz_A[0][1] * matriz_B[1][1] + matriz_A[0][2] * matriz_B[2][1]
+    m13 = matriz_A[0][0] * matriz_B[0][2] + matriz_A[0][1] * matriz_B[1][2] + matriz_A[0][2] * matriz_B[2][2]
+    primeira = [[m11], [m12], [m13]]
+    return primeira
+
+def segunda_linha_3x3(matriz_A, matriz_B):
+    m21 = matriz_A[1][0] * matriz_B[0][0] + matriz_A[1][1] * matriz_B[1][0] + matriz_A[1][2] * matriz_B[2][0]
+    m22 = matriz_A[1][0] * matriz_B[0][1] + matriz_A[1][1] * matriz_B[1][1] + matriz_A[1][2] * matriz_B[2][1]
+    m23 = matriz_A[1][0] * matriz_B[0][2] + matriz_A[1][1] * matriz_B[1][2] + matriz_A[1][2] * matriz_B[2][2]
+    segunda = [[m21], [m22], [m23]]
+    return segunda
+
+def terceira_linha_3x3(matriz_A, matriz_B):
+    m31 = matriz_A[2][0] * matriz_B[0][0] + matriz_A[2][1] * matriz_B[1][0] + matriz_A[2][2] * matriz_B[2][0]
+    m32 = matriz_A[2][0] * matriz_B[0][1] + matriz_A[2][1] * matriz_B[1][1] + matriz_A[2][2] * matriz_B[2][1]
+    m33 = matriz_A[2][0] * matriz_B[0][2] + matriz_A[2][1] * matriz_B[1][2] + matriz_A[2][2] * matriz_B[2][2]
+    terceira = [[m31], [m32], [m33]]
+    return terceira
 
 
