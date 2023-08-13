22853f16d4469eea956b4ec5087adc61098cb27f
ggiffoni <89454421+ggiffoni@users.noreply.github.com>
2023-08-13 08:19:10 -0300

    Multiplicação de Matrizes 3x3 com módulo.

---
+++ Multi Matrix 3x3 Módulo.py
@@ -0,0 +1,15 @@
+#Multiplicação de Matrizes 3x3 com módulo.
+
+from numpy import array
+import Linhas_Colunas_3x3
+
+matriz_A = eval(input("Entre com a sua matriz A:"))
+matriz_B = eval(input("Entre com a sua matriz B:"))
+
+linha1 = Linhas_Colunas_3x3.primeira_linha_3x3(matriz_A, matriz_B)
+linha2 = Linhas_Colunas_3x3.segunda_linha_3x3(matriz_A, matriz_B)
+linha3 = Linhas_Colunas_3x3.terceira_linha_3x3(matriz_A, matriz_B)
+
+print([[linha1], [linha2], [linha3]])
+
+
