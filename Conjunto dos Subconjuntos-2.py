ff6af16977681adb42a2220d94185ab793df2add
ggiffoni <89454421+ggiffoni@users.noreply.github.com>
2023-08-20 12:01:40 -0300

    Acha o conjunto dos subconjuntos de determinado conjunto fornecido. (Versão compreendida)

--- Conjunto dos Subconjuntos-2.py
+++ Conjunto dos Subconjuntos-2.py
@@ -1,3 +1,11 @@
+# Calcula o conjunto dos subconjuntos de determinado conjunto fornecido pelo usuário.
+
+# ENTRADA DE DADOS:
+
+lista = eval(input("Entre com uma lista de números: "))
+
+
+# CÁLCULOS INTERMEDIÁRIOS:
 
 def subconjuntos(numeros):
     resultado = [[]]
@@ -6,7 +14,7 @@ def subconjuntos(numeros):
         resultado += [i + [numero] for i in resultado]
     return resultado
 
-lista = eval(input("Entre com uma lista de números: "))
+# APRESENTAÇÃO DOS RESULTADOS:
 
 conjunto = subconjuntos(lista)
 
